650785ac84aa1dd0179959868293f8328ff21b26
Henrik Löfgren <henrik.lofgren@gmail.com>
2026-07-02 15:33:16 +0200

    Disable interrupts and interrupts generation before booting from SD-card to avoid crashing

--- src/TV80-verilog/tv80s.v
+++ src/TV80-verilog/tv80s.v
@@ -29,7 +29,7 @@ module tv80s (/*AUTOARG*/
   reset_n, clk, wait_n, int_n, nmi_n, busrq_n, di, cen
   );
 
-  parameter Mode = 1;    // 0 => Z80, 1 => Fast Z80, 2 => 8080, 3 => GB
+  parameter Mode = 0;    // 0 => Z80, 1 => Fast Z80, 2 => 8080, 3 => GB
   parameter T2Write = 1; // 0 => wr_n active in T3, /=0 => wr_n active in T2
   parameter IOWait  = 1; // 0 => Single cycle I/O, 1 => Std I/O cycle
 
